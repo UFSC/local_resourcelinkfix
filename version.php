659b437ca62c1eb3ec0786e70ee3f37896d9531e
Roberto Silvino <robertosilvino@gmail.com>
2026-09-23 17:39:43 -0300

    Moodle 4.5: requires 4.5
    - requires = 2024100700 (Moodle 4.5.0, conferido no version.php da tag
      v4.5.0). Testes iguais aos da MOODLE_401_STABLE.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_resourcelinkfix';
 $plugin->version   = 2026092300;
-$plugin->requires  = 2022112800; // Moodle 4.1.
+$plugin->requires  = 2024100700; // Moodle 4.5.
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = '0.1';
